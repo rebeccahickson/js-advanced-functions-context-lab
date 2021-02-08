8faea804958a0e6c60fd8745222457fed59e2977
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-08 11:36:03 -0500

    commit tree b788db8397863811d066d432acb45e6541ec621d

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
-/* Your Code Here */
+function createEmployeeRecord(arr) {
+  let newObject = {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return newObject;
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords(arr) {
+  return arr.map((record) => {
+    return createEmployeeRecord(record);
+  });
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent(dateStamp) {
+  let splitTime = dateStamp.split(" ");
+  let newObj = {
+    type: "TimeIn",
+    hour: parseInt(splitTime[1]),
+    date: splitTime[0],
+  };
+  this.timeInEvents.push(newObj);
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  let splitTime = dateStamp.split(" ");
+  let newObj = {
+    type: "TimeOut",
+    hour: parseInt(splitTime[1]),
+    date: splitTime[0],
+  };
+  this.timeOutEvents.push(newObj);
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  let timeIn = this.timeInEvents.find((x) => {
+    return x.date === date;
+  });
+  let timeOut = this.timeOutEvents.find((x) => {
+    return x.date === date;
+  });
+  let hours = (timeOut.hour - timeIn.hour) / 100;
+  return hours;
+}
+
+function wagesEarnedOnDate(date) {
+  let hoursFn = hoursWorkedOnDate.bind(this);
+  let hours = hoursFn(date);
+  return hours * this.payPerHour;
+}
+
+function calculatePayroll(arr) {
+  return arr.reduce((acc, employee) => {
+    return (acc += allWagesFor.call(employee));
+  }, 0);
+}
+
+function findEmployeeByFirstName(arr, name) {
+  return arr.find((x) => {
+    return x.firstName === name;
+  });
+}
+
+// Break
 
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  let eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  let payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
+  return payable;
+};
